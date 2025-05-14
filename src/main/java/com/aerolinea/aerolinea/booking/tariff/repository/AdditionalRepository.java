5f83803d795fd7c5acc6af8ebb8dc6500505d0be
= <=>
2025-05-14 17:54:01 -0500

    -se realizo ajuste

--- src/main/java/com/aerolinea/aerolinea/booking/tariff/repository/AdditionalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aerolinea.aerolinea.booking.tariff.repository;
-
-import com.aerolinea.aerolinea.booking.tariff.entity.Additional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdditionalRepository extends JpaRepository<Additional, Long> {
-}
